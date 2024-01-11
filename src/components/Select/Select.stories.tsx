bfafea20acb89be37f9c2c7a3638880029011680
gbenjamin <benjamin.gottrant@ynov.com>
2024-01-11 15:39:52 +0100

    Select component

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,16 +1,39 @@
+import { action } from "@storybook/addon-actions";
 import React from "react";
-import  { useState } from 'react';
-import { YDesignWrapper } from "../YDesignWrapper/YDesignWrapper";
 import { Select, SelectProps } from "./Select";
+import { YDesignWrapper } from "../YDesignWrapper/YDesignWrapper";
+import { Meta, StoryObj } from "@storybook/react";
 import { css } from "@emotion/css";
-import { Icon } from "../Icon/Icon";
 
-const StorybookSelect: React.FC<SelectProps> = (props) => {
-    const [selectedValue, setSelectedValue] = useState("");
-  const handleSelect = (value: string) => {
-    setSelectedValue(value);
+const meta: Meta<typeof Select> = {
+    title: "Select",
+    parameters: {
+        backgrounds: { disable: true },
+    },
+    component: (args) => (
+        <YDesignWrapper>
+            <div
+                className={css`
+                    width: 100%;
+                    height: 90vh;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                `}
+            >
+                <Select {...args} />
+            </div>
+        </YDesignWrapper>
+    ),
+};
 
-  };
+type Story = StoryObj<typeof meta>;
+
+export const DefaultSelect: Story = (args: SelectProps) => {
+    const handleSelect = (selectedOption: string) => {
+        action("onSelect")(selectedOption);
+    };
+
     return (
         <YDesignWrapper>
             <div
@@ -22,28 +45,46 @@ const StorybookSelect: React.FC<SelectProps> = (props) => {
                     align-items: center;
                 `}
             >
-                <Select title={"Select an element "} options={["First option" , "Second option", "Third option","1 ", "Fourth option" ]} themeUser={"light"} onSelect={handleSelect} isBlock={true}/>
+                <Select
+                    title={args.title}
+                    isDarkMode={args.isDarkMode}
+                    // onSelect={handleSelect}
+
+                    {...args}
+                />
             </div>
         </YDesignWrapper>
     );
 };
 
-// export default {
-//     title: "Select",
-//     component: StorybookSelect,
-//     parameters: {
-//         backgrounds: { disable: true },
-//     },
-//     argTypes: {
-//         children: { control: "text" },
-//         variant: {
-//             control: "select",
-//             options: ["primary", "secondary"],
-//         },
-//         size: {
-//             control: "select",
-//             options: ["small", "medium", "large"],
-//         },
-//         rounded: { control: "boolean" },
-//     },
-// };
+DefaultSelect.args = {
+    title: "Select an option",
+    options: ["First option", "Second option", "Third option", "Fourth option"],
+    isDarkMode: false,
+    isBlock: true,
+};
+
+DefaultSelect.argTypes = {
+    title: {
+        control: "string",
+    },
+    options: {
+        control: {
+            type: "array",
+            of: { type: "string" },
+        },
+    },
+    isDarkMode: {
+        control: "boolean",
+    },
+    onSelect: {
+        table: {
+            disable: true,
+        },
+    },
+    isBlock: {
+        control: "boolean",
+    },
+};
+
+export default meta;
